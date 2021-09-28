bc2b0c5f47efbe01e7719b35b9086cf8d0f017e6
PANKAJ SHARMA <pjsh18@gmail.com>
2021-09-28 20:42:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etFirstValue, etSecondValue;
+    TextView tvAns;
+    Button add,subtract,multiply,divive;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etFirstValue=findViewById(R.id.etFirstValue);
+        etSecondValue=findViewById(R.id.etSecondValue);
+        tvAns=findViewById(R.id.tvAns);
+
+        add=findViewById(R.id.btnAdd);
+        subtract=findViewById(R.id.btnSubtract);
+        multiply=findViewById(R.id.btnMultiply);
+        divive=findViewById(R.id.btnDevide);
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int firstValue, secondValue, ans;
+                firstValue = Integer.parseInt(etFirstValue.getText().toString());
+                secondValue = Integer.parseInt(etSecondValue.getText().toString());
+                ans= firstValue+ secondValue;
+                tvAns.setText(" Answer is = " +ans);
+
+            }
+        });
+
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float firstValue, secondValue, ans;
+                firstValue = Float.parseFloat(etFirstValue.getText().toString());
+                secondValue =Float.parseFloat(etSecondValue.getText().toString());
+                ans= firstValue- secondValue;
+                tvAns.setText(" Answer is = " +ans);
+
+            }
+        });
+
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int firstValue, secondValue, ans;
+                firstValue = Integer.parseInt(etFirstValue.getText().toString());
+                secondValue = Integer.parseInt(etSecondValue.getText().toString());
+                ans= firstValue* secondValue;
+                tvAns.setText(" Answer is = " +ans);
+
+            }
+        });
+
+
+        divive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float firstValue, secondValue,ans;
+                firstValue = Float.parseFloat(etFirstValue.getText().toString());
+                secondValue = Float.parseFloat(etSecondValue.getText().toString());
+                ans= firstValue/ secondValue;
+                tvAns.setText(" Answer is = " +ans);
+
+            }
+        });
+
+    }
+}
